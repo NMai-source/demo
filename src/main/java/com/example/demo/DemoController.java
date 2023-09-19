efd2f451d7bc7bfd99836c0e99336b8188bfb274
Nakagami Mai <nm30.mmn0kg@gmail.com>
2023-09-19 13:47:52 +0900

    HTML表示

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,27 @@
 package com.example.demo;
 
-public class DemoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("index");
+        mav.addObject("msg", "お名前を入力してください");
+        return mav;
+    }
+
+    @PostMapping("/")
+    public ModelAndView send(
+            @RequestParam("text1")String str,
+            ModelAndView mav) {
+        mav.setViewName("index");
+        mav.addObject("msg", "こんにちは" + str + "さん！");
+        mav.addObject("value", str);
+        return mav;
+    }
 }
